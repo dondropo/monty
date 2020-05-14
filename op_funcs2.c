ff7f86d82ca0ec9ecc33fcc652276a77ec8f272b
David Corredor <vidmore8@gmail.com>
2020-05-13 20:26:55 -0500

    [Upload] - swap

--- op_funcs2.c
+++ op_funcs2.c
@@ -1,21 +1,19 @@
 #include "monty.h"
 
-/* void swap(stack_t **head, unsigned int line_number)
+void swap(stack_t **head, unsigned int line_number)
 {
-	stack_t *temp;
-	int holder;
+	stack_t *temp = *head;
+	int num;
 
-	if (*head != NULL && (*head)->next != NULL)
-	{
-		temp = *head;
-		hold = temp->next;
-		holder = temp->n;
-		temp->n = hold->n;
-		hold->n = holder;
-	}
-	else
+	if (!*head && (*head)->next != NULL)
 	{
 		printf("L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-} */
+	else
+	{
+		num = temp->n;
+		temp->n = temp->next->n;
+		temp->next->n = num;
+	}
+}
