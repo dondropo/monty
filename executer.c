856db028adcbced60eec5a03aa7edd4d7e1ea574
David Corredor <vidmore8@gmail.com>
2020-05-14 13:01:40 -0500

    [Upload] - Uploading new error handler

--- executer.c
+++ executer.c
@@ -70,60 +70,6 @@ void validate_buffer(char *line, size_t line_num, stack_t **h)
 	}
 }
 
-
-
-/**
- * error_handler - Function to handle errors
- * @type_err: number of error
- * @opcode: opcode
- * @line_num: line of error
- * Return: none
- */
-
-void error_handler(int type_err, char *opcode, unsigned int line_num)
-{
-	switch (type_err)
-	{
-	case 1:
-		dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", line_num, opcode);
-		clean_memory();
-		exit(EXIT_FAILURE);
-		break;
-	case 2:
-		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_num);
-		clean_memory();
-		exit(EXIT_FAILURE);
-		break;
-	case 3:
-		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_num);
-		clean_memory();
-		exit(EXIT_FAILURE);
-		break;
-	case 4:
-		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_num);
-		clean_memory();
-		exit(EXIT_FAILURE);
-		break;
-	default:
-		break;
-	}
-}
-
-/**
- * clean_memory - memory cleaner
- * Return: none
- */
-
-void clean_memory(void)
-{
-	if (global.buffer)
-		free(global.buffer);
-	if (global.file_des)
-		fclose(global.file_des);
-	if (global.head)
-		free_dlistint(global.head);
-}
-
 /**
  * executer - execute function
  * @h: stack_t pointer
--- monty_bytecodes/00.m
+++ monty_bytecodes/00.m
@@ -1,7 +1,8 @@
+
 push 1
+
 push 2
 push 3
 pall
-push alo
-pint
-alo
+swap
+pall
