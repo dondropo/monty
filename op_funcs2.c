774d4c6e5fa3d9191ed4fa5c5f53116027312c82
David Corredor <vidmore8@gmail.com>
2020-05-13 21:15:42 -0500

    [Upload] - nope

--- op_funcs2.c
+++ op_funcs2.c
@@ -16,15 +16,7 @@ void swap(stack_t **head, unsigned int line_number)
 		temp->n = temp->next->n;
 		temp->next->n = num;
 	}
-} 
-
-/**
- * exec_push - Execute the opcode push
- * @head: pointer to head of the stack
- * @line_num: number of the line
- *
- * Return: none
- */
+}
 void pop_element(stack_t **head, unsigned int line_num)
 {
 	stack_t *tmp;
@@ -34,10 +26,16 @@ void pop_element(stack_t **head, unsigned int line_num)
 		dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line_num);
 		exit(EXIT_FAILURE);
 	}
-	tmp = *head;
-	*head = (*head)->next;
-	(*head)->prev = NULL;
-	free(tmp);
+	else
+	{
+		tmp = *head;
+	    if (tmp->next != NULL)
+	    {
+	    	tmp->next->prev = NULL;
+	    }
+	    *head = (*head)->next;
+	    free(tmp);
+	}
 }
 
 /**
