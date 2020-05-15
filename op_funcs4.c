b466b27a93df74a82d2bc52fca999b2d068730a6
David Corredor <vidmore8@gmail.com>
2020-05-14 21:28:06 -0500

    [Upload] -rotr function

--- op_funcs4.c
+++ op_funcs4.c
@@ -35,17 +35,16 @@ void rotr(stack_t **head, unsigned int line_num)
 	stack_t *temp = *head, *new;
 
 	(void)line_num;
+
 	if (!temp)
 		return;
+
 	while (temp->next)
 		temp = temp->next;
 
-	new = malloc(sizeof(stack_t));
-	if (!new)
-		return;
-
-	new->n = temp->n;
-	new->next = (*head);
-	new->prev = NULL;
-	(*head) = new;
+	temp->prev->next = NULL;
+	temp->prev = NULL;
+	temp->next = (*head);
+	(*head)->prev = temp;
+	*head = temp;
 }
